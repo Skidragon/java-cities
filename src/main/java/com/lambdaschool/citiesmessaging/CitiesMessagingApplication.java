d92b11c56e5c3ab5773a0ff1a3dd73535dc8c116
Simon Davis <simon_freelance@hotmail.com>
2019-02-13 16:58:40 -0500

    Initial setup.

---
+++ src/main/java/com/lambdaschool/citiesmessaging/CitiesMessagingApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.citiesmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiesMessagingApplication.class, args);
+    }
+
+}
+
